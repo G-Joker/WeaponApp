14501fc7a824c7667a5189fb5ba78fa78d4a4bff
WeaponZhi <584098488@qq.com>
2017-09-08 19:00:26 +0800

    add ViewBindingAdapter

---
+++ lib.mvvm/src/main/java/com/weapon/joker/lib/mvvm/common/ViewBindingAdapter.java
@@ -0,0 +1,11 @@
+package com.weapon.joker.lib.mvvm.common;
+
+/**
+ * ViewBindingAdapter 放置一些公用的 @BindingAdapter
+ * author:张冠之
+ * time: 2017/9/8 下午7:00
+ * e-mail: guanzhi.zhang@sojex.cn
+ */
+
+public class ViewBindingAdapter {
+}
