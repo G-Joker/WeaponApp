1744e8a4da4f46dbbabbe7eb4c3e8bf266ea4db1
WeaponZhi <584098488@qq.com>
2017-09-08 18:30:31 +0800

    add databinding

--- app.home/src/main/java/com/weapon/joker/app/home/HomeActivity.java
+++ app.home/src/main/java/com/weapon/joker/app/home/HomeActivity.java
@@ -1,7 +1,7 @@
 package com.weapon.joker.app.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
