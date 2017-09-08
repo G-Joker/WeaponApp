ad41d52ae9c3a9d5399c8f3868b0aef8684031a8
WeaponZhi <584098488@qq.com>
2017-09-08 16:56:52 +0800

    add Small framework

---
+++ app/src/main/java/com/weaponzhi/weaponapp/application/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.weaponzhi.weaponapp.application;
+
+import android.app.Application;
+
+import net.wequick.small.Small;
+
+/**
+ * Created by WeaponZhi on 2017/9/8.
+ */
+
+public class BaseApplication extends Application{
+
+    public BaseApplication(){
+        Small.preSetUp(this);
+    }
+}
