5d9e6b440bfbf88badf0b5aedc12c0cf2b2cbbde
WeaponZhi <584098488@qq.com>
2017-09-08 19:07:16 +0800

    add home_function

---
+++ app.home/src/main/java/com/weapon/joker/app/Function.java
@@ -0,0 +1,18 @@
+package com.weapon.joker.app;
+import com.weapon.joker.app.home.*;
+
+/**
+ * Function 业务索引
+ * author:张冠之
+ * time: 2017/9/8 下午7:01
+ * e-mail: guanzhi.zhang@sojex.cn
+ */
+
+interface Function {
+    /**
+     * {@link HomeActivity 首页}
+     */
+    void activity();
+
+    void fragment();
+}
