99ea3ac3ab54846023d13826a0523020a2c0bfb8
hiyangyue <hi.yangyue1993@gmail.com>
2017-10-11 21:25:30 +0800

    [feat] 添加QQ Auth登陆

---
+++ Client/app.mine/src/main/java/com/weapon/joker/app/mine/login/QQAuthLoginActivity.java
@@ -0,0 +1,175 @@
+package com.weapon.joker.app.mine.login;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.weapon.joker.app.mine.R;
+import com.weapon.joker.lib.middleware.utils.Constants;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * author : yueyang
+ * date : 2017/10/11
+ */
+public class QQAuthLoginActivity extends Activity {
+
+    private WebView mWebView;
+    public static String mUrl;
+    public static final int QQ_AUTHORITY_REQUEST = 2001;
+    private QQListener mQQListener = new QQListener() {
+        @Override
+        public void onComplete(String uid, String access_token) {
+
+        }
+
+        @Override
+        public void onError() {
+
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+    };
+
+    public static void launch(Activity activity) {
+        if (activity == null) return;
+        Intent intent = new Intent(activity, QQAuthLoginActivity.class);
+        String encodeUrl, url = "";
+        try {
+            encodeUrl = URLEncoder.encode(Constants.QQ_REDIRECT_URL, "utf-8");
+            url = Constants.QQ_URL_OAUTH2_AUTHORIZATION_CODE;
+            url = url.replace("[YOUR_APPID]", Constants.QQ_APP_ID);
+            url = url.replace("[YOUR_REDIRECT_URI]", encodeUrl);//简书重定向链接
+            url = url.replace("[THE_SCOPE]", Constants.QQ_SCOPE);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        QQAuthLoginActivity.mUrl = url;
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        activity.startActivityForResult(intent, QQ_AUTHORITY_REQUEST);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_login);
+
+        initWebView();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initWebView() {
+        mWebView = findViewById(R.id.webview);
+        mWebView.setVerticalScrollBarEnabled(false);
+        mWebView.setHorizontalScrollBarEnabled(false);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setSaveFormData(false);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new QQWebViewClient());
+        mWebView.loadUrl(mUrl);
+        mWebView.requestFocus();
+    }
+
+    private class QQWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.startsWith(Constants.QQ_REDIRECT_URL)) {
+                try {
+                    handlerLoginUrl(url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            if (isFinishing()) {
+                return;
+            }
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (isFinishing()) {
+                return;
+            }
+            mWebView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void handlerLoginUrl(String url) throws IOException {
+        if (TextUtils.isEmpty(url))
+            return;
+        String newUrl = url.replace("?#", "=");
+        newUrl = newUrl.replace("&", "=");
+        String[] params = newUrl.split("=");
+        String mAccessToken = params[2];//
+        if (!TextUtils.isEmpty(mAccessToken))
+            getOpenIdByAccessToken(mAccessToken);
+        else
+            mQQListener.onError();
+    }
+
+    private void getOpenIdByAccessToken(String token) {
+        if (!TextUtils.isEmpty(token))
+            throw new IllegalArgumentException("accessToken cannot be null");
+        String url = Constants.QQ_URL_OAUTH2_OPENID;
+        url = url.replace("[YOUR_ACCESS_TOKEN]", token);
+        Uri.Builder urlBuilder = Uri.parse(url).buildUpon();
+        final String realUrl = urlBuilder.build().toString();
+        // todo import okhttp
+//        Request.Builder requestBuilder = new Request.Builder().url(realUrl).get();
+//        final Request request = requestBuilder.build();
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//        final Call call = okHttpClient.newCall(request);
+//        call.enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                mQQListener.onError();
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                ResponseBody body = response.body();
+//                String respStr = new String(body.bytes(), "utf-8");
+//                int start = respStr.indexOf("{");
+//                int end = respStr.indexOf("}") + 1;
+//                String substring = respStr.substring(start, end);
+//                // TODO: 2017/10/11 parse response
+//            }
+//        });
+    }
+
+    private interface QQListener {
+        void onComplete(String uid, String access_token);
+
+        void onError();
+
+        void onCancel();
+    }
+}
--- Client/lib.middleware/src/main/java/com/weapon/joker/lib/middleware/utils/Constants.java
+++ Client/lib.middleware/src/main/java/com/weapon/joker/lib/middleware/utils/Constants.java
@@ -12,6 +12,17 @@ package com.weapon.joker.lib.middleware.utils;
 
 public interface Constants {
     String QQ_APP_ID = "101426105";
+    String QQ_SCOPE = "get_user_info";
+    String QQ_REDIRECT_URL = "weapon.com"; // todo 换成真实的重定向链接
+    String QQ_URL_OAUTH2_AUTHORIZATION_CODE = "https://graph.qq.com/oauth2.0/authorize?response_type=token&display=mobile&client_id=[YOUR_APPID]&redirect_uri=[YOUR_REDIRECT_URI]&scope=[THE_SCOPE]";
+    String QQ_URL_OAUTH2_ACCESSTOKEN = "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=[YOUR_APPID]&client_secret=[YOUR_APP_Key]&code=[The_AUTHORIZATION_CODE]&state=test&redirect_uri=[YOUR_REDIRECT_URI]";
+    String QQ_URL_OAUTH2_OPENID = "https://graph.qq.com/oauth2.0/me?access_token=[YOUR_ACCESS_TOKEN]";
+    String QQ_URL_GET_USER_INFO = "https://graph.qq.com/user/get_user_info?access_token=[YOUR_ACCESS_TOKEN]&oauth_consumer_key=[YOUR_APPID]&openid=[YOUR_OPENID]";
+
+
+
+
+
 
 
 }
