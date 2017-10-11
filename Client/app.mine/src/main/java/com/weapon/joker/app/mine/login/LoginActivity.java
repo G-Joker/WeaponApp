908eba009cd40076993f400aa0bea7f991b4c9ea
xiaweizi <1012126908@qq.com>
2017-10-11 21:36:09 +0800

    [func] 完成登录注册界面并实现转场动画的跳转

--- Client/app.mine/src/main/java/com/weapon/joker/app/mine/login/LoginActivity.java
+++ Client/app.mine/src/main/java/com/weapon/joker/app/mine/login/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.weapon.joker.app.mine.login;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.support.design.widget.FloatingActionButton;
 import android.transition.Explode;
 import android.transition.Transition;
+import android.view.View;
+import android.widget.TextView;
 
 import com.weapon.joker.app.mine.R;
 import com.weapon.joker.lib.mvvm.common.BaseActivity;
@@ -13,7 +19,10 @@ import com.weapon.joker.lib.mvvm.common.BaseActivity;
  * e-mail:   1012126908@qq.com
  * desc:
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    private FloatingActionButton mButton;
+    private TextView mTvForgetPassword;
 
     @Override
     public int getLayoutId() {
@@ -22,11 +31,24 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        mButton = findViewById(R.id.fab);
+        mTvForgetPassword = findViewById(R.id.tv_forget_password);
+        mTvForgetPassword.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        mButton.setOnClickListener(this);
     }
 
     @Override
     protected Transition getTransition() {
         return new Explode();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, mButton, mButton.getTransitionName());
+                startActivity(new Intent(this, RegisterActivity.class), options.toBundle());
+                break;
+        }
     }
 }
--- Client/app.mine/src/main/java/com/weapon/joker/app/mine/login/RegisterActivity.java
+++ Client/app.mine/src/main/java/com/weapon/joker/app/mine/login/RegisterActivity.java
@@ -1,5 +1,18 @@
 package com.weapon.joker.app.mine.login;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.CardView;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateInterpolator;
+
+import com.weapon.joker.app.mine.R;
+import com.weapon.joker.lib.mvvm.common.BaseActivity;
+
 /**
  * class：   Client
  * author：  xiaweizi
@@ -7,6 +20,114 @@ package com.weapon.joker.app.mine.login;
  * e-mail:   1012126908@qq.com
  * desc:
  */
-public class RegisterActivity {
+public class RegisterActivity extends BaseActivity {
 
+    private CardView             mCardView;
+    private FloatingActionButton mFloatingActionButton;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    public void initView() {
+        mCardView = findViewById(R.id.cv_add);
+        mFloatingActionButton = findViewById(R.id.fab);
+        ShowEnterAnimation();
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateRevealClose();
+            }
+        });
+    }
+
+    private void ShowEnterAnimation() {
+        Transition transition = TransitionInflater.from(this).inflateTransition(R.transition.fabtransition);
+        getWindow().setSharedElementEnterTransition(transition);
+
+        transition.addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                mCardView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                transition.removeListener(this);
+                animateRevealShow();
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+
+
+        });
+    }
+
+    public void animateRevealShow() {
+        Animator mAnimator = ViewAnimationUtils.createCircularReveal(mCardView,
+                                                                     mCardView.getWidth() / 2,
+                                                                     0,
+                                                                     mFloatingActionButton.getWidth() / 2,
+                                                                     mCardView.getHeight());
+        mAnimator.setDuration(500);
+        mAnimator.setInterpolator(new AccelerateInterpolator());
+        mAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mCardView.setVisibility(View.VISIBLE);
+                super.onAnimationStart(animation);
+            }
+        });
+        mAnimator.start();
+    }
+
+    public void animateRevealClose() {
+        Animator mAnimator = ViewAnimationUtils.createCircularReveal(mCardView,
+                                                                     mCardView.getWidth() / 2,
+                                                                     0,
+                                                                     mCardView.getHeight(),
+                                                                     mFloatingActionButton.getWidth() / 2);
+        mAnimator.setDuration(500);
+        mAnimator.setInterpolator(new AccelerateInterpolator());
+        mAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCardView.setVisibility(View.INVISIBLE);
+                super.onAnimationEnd(animation);
+                mFloatingActionButton.setImageResource(R.mipmap.ic_mine_register);
+                RegisterActivity.super.onBackPressed();
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+            }
+        });
+        mAnimator.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        animateRevealClose();
+    }
 }
