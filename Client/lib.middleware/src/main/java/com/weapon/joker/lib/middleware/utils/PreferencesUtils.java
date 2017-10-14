8893aeb3efa3444104b40c0707b99f432e531c4a
hiyangyue <hi.yangyue1993@gmail.com>
2017-10-14 21:04:17 +0800

    [func] 添加SharedPreferences工具类

---
+++ Client/lib.middleware/src/main/java/com/weapon/joker/lib/middleware/utils/PreferencesUtils.java
@@ -0,0 +1,68 @@
+package com.weapon.joker.lib.middleware.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Preferences工具类
+ * author : yueyang
+ * email : hi.yangyue1993@gmail.com
+ * date : 2017/10/14
+ */
+public class PreferencesUtils {
+
+    private static final String PREFERENCE_NAME = "Weapon_setting";
+
+
+    private PreferencesUtils() {
+        throw new AssertionError();
+    }
+
+    public static boolean putString(Context context, String key, String value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        return editor.commit();
+    }
+
+    public static String getString(Context context, String key, String defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getString(key, defaultValue);
+    }
+
+    public static int getInt(Context context, String key, int defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getInt(key, defaultValue);
+    }
+
+    public static boolean putInt(Context context, String key, int value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(key, value);
+        return editor.commit();
+    }
+
+    public static boolean putLong(Context context, String key, long value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putLong(key, value);
+        return editor.commit();
+    }
+
+    public static long getLong(Context context, String key, long defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getLong(key, defaultValue);
+    }
+
+    public static boolean putBoolean(Context context, String key, boolean value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(key, value);
+        return editor.commit();
+    }
+
+    public static boolean getBoolean(Context context, String key, boolean defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getBoolean(key, defaultValue);
+    }
+}
