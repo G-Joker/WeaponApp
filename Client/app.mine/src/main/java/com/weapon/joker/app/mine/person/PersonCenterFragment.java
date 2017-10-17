7301019155f4bd5572882a7cbd493dd0e94c611a
xiaweizi <1012126908@qq.com>
2017-10-17 10:47:15 +0800

    [func] 个人中心添加注销登录功能

--- Client/app.mine/src/main/java/com/weapon/joker/app/mine/person/PersonCenterFragment.java
+++ Client/app.mine/src/main/java/com/weapon/joker/app/mine/person/PersonCenterFragment.java
@@ -1,7 +1,11 @@
 package com.weapon.joker.app.mine.person;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 import com.weapon.joker.app.mine.BR;
 import com.weapon.joker.app.mine.R;
+import com.weapon.joker.app.mine.databinding.FragmentPersonCenterBinding;
 import com.weapon.joker.lib.mvvm.common.BaseFragment;
 
 /**
@@ -14,7 +18,9 @@ import com.weapon.joker.lib.mvvm.common.BaseFragment;
  * </pre>
  */
 
-public class PersonCenterFragment extends BaseFragment<PersonCenterViewModel, PersonCenterModel> implements PersonCenterContact.View{
+public class PersonCenterFragment extends BaseFragment<PersonCenterViewModel, PersonCenterModel> implements PersonCenterContact.View {
+
+    private FragmentPersonCenterBinding mDataBinding;
 
     @Override
     public int getLayoutId() {
@@ -23,7 +29,24 @@ public class PersonCenterFragment extends BaseFragment<PersonCenterViewModel, Pe
 
     @Override
     public void initView() {
+        mDataBinding = ((FragmentPersonCenterBinding) getViewDataBinding());
+        setToolbar();
+        getViewModel().setHeaderData();
+    }
 
+    /**
+     * Toolbar 相关设置
+     */
+    private void setToolbar() {
+        ((AppCompatActivity) getActivity()).setSupportActionBar(mDataBinding.toolbar);
+        // 设置 toolbar 具有返回按钮
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDataBinding.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
     }
 
     @Override
--- Client/app.mine/src/main/java/com/weapon/joker/app/mine/person/PersonCenterViewModel.java
+++ Client/app.mine/src/main/java/com/weapon/joker/app/mine/person/PersonCenterViewModel.java
@@ -1,6 +1,13 @@
 package com.weapon.joker.app.mine.person;
 
+import android.content.DialogInterface;
 import android.databinding.Bindable;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.weapon.joker.app.mine.BR;
+import com.weapon.joker.lib.net.data.UserData;
 
 /**
  * <pre>
@@ -15,6 +22,43 @@ import android.databinding.Bindable;
 public class PersonCenterViewModel extends PersonCenterContact.ViewModel {
 
     @Bindable
-    public String testString = "个人中心";
+    public String userName;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+        notifyPropertyChanged(BR.userName);
+    }
+
+    /**
+     * 设置个人中心头部数据
+     */
+    public void setHeaderData() {
+        String userName = UserData.getInstance().getUserBean(getContext().getApplicationContext()).user;
+        if (!TextUtils.isEmpty(userName)) {
+            setUserName(userName);
+        }
+    }
+
+    /**
+     * 注销登录点击处理
+     *
+     * @param view
+     */
+    public void onLogoutClick(View view) {
+        new AlertDialog.Builder(getContext())
+                .setTitle("提示")
+                .setMessage("确定退出登录？")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 清楚本地数据
+                        UserData.getInstance().clearUserData(getContext());
+                        getView().finish();
+                    }
+                })
+                .setNegativeButton("取消", null)
+                .show();
+
+    }
 
 }
