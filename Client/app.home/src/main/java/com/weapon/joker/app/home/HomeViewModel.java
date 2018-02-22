0f52ac49ec7eaa59f3ddd60aa4453086b3b497e7
WeaponZhi <584098488@qq.com>
2018-02-22 10:49:41 +0800

    [func] 添加首页布局框架

--- Client/app.home/src/main/java/com/weapon/joker/app/home/HomeViewModel.java
+++ Client/app.home/src/main/java/com/weapon/joker/app/home/HomeViewModel.java
@@ -1,13 +1,17 @@
 package com.weapon.joker.app.home;
 
+import android.view.View;
+
 import com.weapon.joker.lib.middleware.utils.LogUtils;
 import com.weapon.joker.lib.net.BaseObserver;
 
 /**
- * Created by WeaponZhi on 2018/2/20.
+ * HomeViewModel 首页Fragment ViewModel
+ * author:张冠之
+ * time: 2018/2/22 上午10:47
+ * e-mail: guanzhi.zhang@sojex.cn
  */
-
-public class HomeViewModel extends HomeContact.ViewModel{
+public class HomeViewModel extends HomeContact.ViewModel {
 
 
     @Override
@@ -15,13 +19,25 @@ public class HomeViewModel extends HomeContact.ViewModel{
         getModel().getHomeListData().subscribe(new BaseObserver<HomeBean>() {
             @Override
             protected void onSuccess(HomeBean entry) throws Exception {
-                LogUtils.i("onSuccess",entry.toString());
+                LogUtils.i("onSuccess", entry.toString());
             }
 
             @Override
             protected void onFailure(Throwable e, boolean isNetWorkError) throws Exception {
-                LogUtils.i("onFailure",e.getMessage());
+                LogUtils.i("onFailure", e.getMessage());
             }
         });
+    }
+
+    public void onQrcodeClick(View view) {
+        LogUtils.i("qrcode is clicked");
+    }
+
+    public void onCategoryClick(View view) {
+        LogUtils.i("category is clicked");
+    }
+
+    public void onSearchTextClick(View view) {
+        LogUtils.i("search is clicked");
     }
 }
--- Client/lib.net/src/main/java/com/weapon/joker/lib/net/ApiConstants.java
+++ Client/lib.net/src/main/java/com/weapon/joker/lib/net/ApiConstants.java
@@ -18,7 +18,7 @@ package com.weapon.joker.lib.net;
 
 public class ApiConstants {
 
-    public static final String HOME_HOST = "";
+    public static final String HOME_HOST = "http://www.baidu.com/";
     public static final String MESSAGE_HOST = "http://fy.iciba.com/";
     public static final String MINE_HOST = "http://47.95.198.174/api/";
 
